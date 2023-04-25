66b7d5804ad4627b85c17e8da4559d2cb4887dfe
curtyraissa <raissacurty@gmail.com>
2023-04-25 17:13:35 -0300

    ajuste apiAuth

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "http://localhost:5000" || process.env.BASE_URL ;
 
 function signIn(body) {
   const promise = axios.post(`${BASE_URL}/`, body);
